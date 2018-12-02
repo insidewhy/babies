ef8bcce899f35f6855ebaea118751a7d97ed4dc0
James Pike <jpike@chilon.net>
2018-12-02 22:15:50 +0000

    implement --convert command

--- libshow/db.py
+++ libshow/db.py
@@ -8,6 +8,13 @@ def __load_yaml_file(filepath):
         except yaml.YAMLError as err:
             raise ValueError(*err.args)
 
+def dump_yaml_file(filepath, data):
+    with open(filepath, 'w') as stream:
+        try:
+            return yaml.safe_dump(data, stream, default_flow_style=False)
+        except yaml.YAMLError as err:
+            raise ValueError(*err.args)
+
 class Db:
     def __init__(self):
         self.__series_db = []
@@ -23,12 +30,22 @@ class Db:
         with open(filepath, 'r') as fp:
             for _, line in enumerate(fp):
                 line = line.rstrip('\n')
-                print('TODO: load line into DB', line)
-
-        print('debug: ', self.__series_db)
+                video_data = {}
+                if line[0] == '*':
+                    start = 'unknown'
+                    space_idx = line.index(' ')
+                    if line[1] != ' ':
+                        start = line[1:space_idx].replace('-', '/').replace('~', ' ') + ' at 00:00'
+                    video_file = line[space_idx + 1:]
+                    video_data['video'] = video_file
+                    video_data['viewings'] = [ { 'start': start, 'end': 'finished?' } ]
+                else:
+                    video_data['video'] = line
+                self.__series_db.append(video_data)
 
     def write_series(self, dirpath):
-        print('TODO: write current ', dirpath)
+        filepath = self.get_series_db_path(dirpath)
+        dump_yaml_file(filepath, self.__series_db)
 
     def get_series_db_path(self, dirpath):
         return os.path.join(dirpath, '.showlist.yaml')
