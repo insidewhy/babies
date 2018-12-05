86be58ed1639044e7fd27aa304b17844cef0f08b
James Pike <jpike@chilon.net>
2018-12-05 00:24:59 +0000

    implement --convert-global command

--- libshow/db.py
+++ libshow/db.py
@@ -19,9 +19,34 @@ def _dump_yaml_file(filepath, data):
         except yaml.YAMLError as err:
             raise ValueError(*err.args)
 
+def _load_old_db(filepath, global_variation):
+    db_entries = []
+    with open(filepath, 'r') as fp:
+        for _, line in enumerate(fp):
+            line = line.rstrip('\n')
+            video_data = {}
+
+            if global_variation or line[0] == '*':
+                if not global_variation:
+                    # remove the *
+                    line = line[1:]
+
+                start = 'sometime'
+                space_idx = line.index(' ')
+                if line[0] != ' ':
+                    start = line[:space_idx].replace('-', '/').replace('~', ' ')
+                video_file = line[space_idx + 1:]
+                video_data['video'] = video_file
+                video_data['viewings'] = [ { 'start': start, 'end': 'sometime at finished?' } ]
+            else:
+                video_data['video'] = line
+            db_entries.append(video_data)
+    return db_entries
+
 class Db:
     def __init__(self):
         self.__series_db = []
+        self.__db = []
 
     def load_series(self, dirpath):
         db_path = self.get_series_db_path(dirpath)
@@ -31,22 +56,9 @@ class Db:
         filepath = os.path.join(dirpath, '.showlist')
         if not os.path.isfile(filepath):
             raise ValueError('database does not exist in this directory')
-
-        with open(filepath, 'r') as fp:
-            for _, line in enumerate(fp):
-                line = line.rstrip('\n')
-                video_data = {}
-                if line[0] == '*':
-                    start = 'sometime'
-                    space_idx = line.index(' ')
-                    if line[1] != ' ':
-                        start = line[1:space_idx].replace('-', '/').replace('~', ' ')
-                    video_file = line[space_idx + 1:]
-                    video_data['video'] = video_file
-                    video_data['viewings'] = [ { 'start': start, 'end': 'sometime at finished?' } ]
-                else:
-                    video_data['video'] = line
-                self.add_show_to_series(video_data)
+        old_entries = _load_old_db(filepath, False)
+        for video_data in old_entries:
+            self.add_show_to_series(video_data)
 
     def get_next_in_series(self):
         for show in self.__series_db:
@@ -73,3 +85,25 @@ class Db:
     def get_series_db_path(self, dirpath):
         return os.path.join(dirpath, '.videos.yaml')
 
+    def add_show_to_global_record(self, video_data):
+        self.__db.append(video_data)
+
+    def load_global_record_v0(self):
+        old_db_path = os.path.expanduser('~/.showtimes')
+        old_entries = _load_old_db(old_db_path, True)
+        for video_data in old_entries:
+            new_entry = video_data.copy()
+
+            # convert viewings to simpler form for global log
+            new_entry.pop('viewings', None)
+            viewings = video_data.get('viewings', None)
+            if viewings:
+                new_entry['start'] = viewings[0]['start']
+
+            self.__db.append(new_entry)
+
+    def write_global_record(self):
+        _dump_yaml_file(self.get_global_record_db_path(), self.__db)
+
+    def get_global_record_db_path(self):
+        return os.path.expanduser('~/.videorecord.yaml')
