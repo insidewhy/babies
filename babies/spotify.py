58c974bb8ecae306712c819433aa86eee599bdff
James Pike <jpike@chilon.net>
2020-05-20 02:55:04 +0800

    format spotify track results

--- babies/spotify.py
+++ babies/spotify.py
@@ -38,6 +38,25 @@ def search_spotify(config: Config, search_terms: List[str], limit=50, raw=False)
         yaml.dump(results.json(), sys.stdout)
         return
 
-    # TODO: format results
-    yaml.dump(results.json(), sys.stdout)
+    yaml.dump(_format_spotify_results(results.json()), sys.stdout)
 
+
+def _format_spotify_results(results):
+    outputs = []
+    for album in results['albums']['items']:
+        # TODO:
+        pass
+
+    for track in results['tracks']['items']:
+        artists = list(map(lambda a: a['name'], track['artists']))
+        output = {
+            'type': 'track',
+            'artist': artists[0],
+            'contributors': artists[1:],
+            'album': track['album']['name'],
+            'name': track['name'],
+            'track_number': track['track_number'],
+        }
+        outputs.append(output)
+
+    return outputs
